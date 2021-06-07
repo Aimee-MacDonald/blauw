9eaecd4c5a8f8ca0ea7b6216103a394044953cef
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-06-07 16:59:56 +0200

    ControlLayer snapshot testing

---
+++ src/tests/components/ControlLayer.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import ControlLayer from '../../components/ControlLayer'
+
+test('Render default Control Layer', () => {
+  const wrapper = shallow(<ControlLayer/>)
+  expect(wrapper.getElement()).toMatchSnapshot()
+})
