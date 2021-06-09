57fc00d26ff71e8f0e8494b38d55d04630badd07
Aimée <aimeelmacdonald@gmail.com>
2021-06-09 17:11:13 +0200

    Edit Booking snapshot update

--- src/tests/components/modals/EditBooking.test.js
+++ src/tests/components/modals/EditBooking.test.js
@@ -4,12 +4,31 @@ import {shallow} from 'enzyme'
 import {EditBooking} from '../../../components/modals/EditBooking'
 
 test(`Render 'Edit Booking' Modal`, () => {
+  const bookingDetails = {
+    _id: '485508e8-8cc9-418d-bc79-0b183964c99f',
+    booking_name: 'A',
+    checkin_date: 1623016800000,
+    room: 0,
+    nights: 10
+  }
+
+  const rooms = [{
+    roomName: 'camping',
+    groupName: 'Camping',
+    maxPax: 80
+  }, {
+    roomName: 'safari_tent_1',
+    groupName: 'Safari Tents',
+    maxPax: 2
+  }, {
+    roomName: 'safari_tent_2',
+    groupName: 'Safari Tents',
+    maxPax: 2
+  }]
+
   const wrapper = shallow(<EditBooking
-    _id={'485508e8-8cc9-418d-bc79-0b183964c99f'}
-    booking_name={'A'}
-    checkin_date={1623016800000}
-    room={0}
-    nights={10}
+    bookingDetails={bookingDetails}
+    rooms={rooms}
   />)
 
   expect(wrapper.getElement()).toMatchSnapshot()
