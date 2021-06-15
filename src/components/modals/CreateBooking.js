e02588ac643c959d6613579ece20b65841978a7f
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-06-15 09:19:12 +0200

    Upgrading Create Booking Modal

--- src/components/modals/CreateBooking.js
+++ src/components/modals/CreateBooking.js
@@ -1,97 +1,83 @@
-import React, {useContext} from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import {connect} from 'react-redux'
 
-import {ServerConnectionContext} from '../../util/ServerConnection'
+const CreateBooking = () => (
+  <StyledCreateBooking>
+    <h1>Create Booking</h1>
 
-import {setModal} from '../../state_management/actions/modal'
-import {createBooking} from '../../state_management/actions/bookings'
-import {normaliseDate} from '../../util/util'
+    <div id='labeled_booking_name'>
+      <label htmlFor='booking_name'>Booking name:</label>
+      <input id='booking_name'/>
+    </div>
 
-export const CreateBooking = props => {
-  const connection = useContext(ServerConnectionContext)
+    <div id='accommodation_details'>
+      <p>Room</p>
+      <p>Guest</p>
+      <p>Arrival</p>
+      <p>Departure</p>
+      <p>Nights</p>
+      <p>Price</p>
+    </div>
 
-  const saveBooking = e => {
-    e.preventDefault()
+    <button>Add Room</button>
+    <button>Add Guest</button>
 
-    const bookingDetails = {
-      booking_name: e.target.name.value,
-      checkin_date: normaliseDate(e.target.date.value),
-      room: parseInt(e.target.room.value),
-      nights: parseInt(e.target.nights.value)
-    }
+    <p id='total'>Total: R</p>
 
-    const createBookingAction = createBooking(bookingDetails)
+    <textarea/>
 
-    props.dispatch(createBookingAction)
-    connection.dispatch({reducer: 'bookings', action: createBookingAction})
-    props.dispatch(setModal())
-  }
-
-  return(
-    <StyledCreateBooking onSubmit={saveBooking}>
-      <h1>Create Booking</h1>
-
-      <label htmlFor='name'>Name:</label>
-      <input id='name' />
-
-      <label htmlFor='date'>Date:</label>
-      <input id='date' type='date' />
-
-      <label htmlFor='room'>Room:</label>
-      <select id='room'>
-        {props.rooms.map((room, index) => <option value={index} key={room.roomName}>{`${room.roomName} - ${index}`}</option>)}
-      </select>
-
-      <label htmlFor='nights'>Nights:</label>
-      <input id='nights' />
-      
-      <button type='reset' onClick={() => props.dispatch(setModal())}>Cancel</button>
-      <button type='submit'>Save</button>
-    </StyledCreateBooking>
-  )
-}
+    <div id='form_controls'>
+      <button>Create Booking</button>
+      <button>Cancel</button>
+    </div>
+  </StyledCreateBooking>
+)
 
 const StyledCreateBooking = styled.form`
+  background-color: cornflowerblue;
+  max-width 64rem;
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  padding: 1.6rem;
+  grid-template-columns: repeat(6, 1fr);
+  padding: 2rem;
 
   h1{
     grid-column-start: 1;
     grid-column-end: 3;
-    margin-bottom: 3.2rem;
-    text-align: center;
   }
 
-  label{
-    text-align: right;
-    margin-top: 0.4rem;
-    padding-right: 0.8rem;
+  #labeled_booking_name{
+    grid-column-start: 1;
+    grid-column-end: 3;
+    margin: 1rem 0;
   }
 
-  input{
-    background-color: transparent;
-    border: none;
-    border-bottom 1px solid black;
-    width: 75%;
-    margin-top: 0.4rem;
+  #accommodation_details{
+    grid-column-start: 1;
+    grid-column-end: 7;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    height: 8rem;
+    justify-items: center;
+    margin-top: 1rem;
   }
 
-  select{
-    background-color: transparent;
-    border: none;
-    border-bottom 1px solid black;
-    width: 75%;
-    margin-top: 0.4rem;
+  #total{
+    grid-column-start: 6;
+    grid-column-end: 7;
+    justify-self: center;
   }
 
-  button{
-    padding: 0.4rem;
-    margin: 3.2rem 0.1rem 0 0.2rem;
+  textarea{
+    grid-column-start: 1;
+    grid-column-end: 7;
+    height: 8rem;
+    margin: 1rem 0;
+  }
+
+  #form_controls{
+    grid-column-start: 3;
+    grid-column-end: 5;
   }
 `
 
-const mapStateToProps = ({rooms}) => ({rooms})
-
-export default connect(mapStateToProps)(CreateBooking)
+export default CreateBooking
